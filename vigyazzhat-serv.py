ed5de31d4ae2f0c1dc1a805e0079e5c0201e4f63
Riccsard <58710642+Riccsard@users.noreply.github.com>
2024-11-14 08:46:44 +0100

    stage1done

--- vigyazzhat-serv.py
+++ vigyazzhat-serv.py
@@ -1,44 +1,111 @@
 from flask import Flask
+from flask import request
 import pymongo
-import uuid
+import time
+import hashlib
+from bson import ObjectId
 
 app = Flask(__name__)
 
 con = pymongo.MongoClient("mongodb://localhost:27017/")
 db = con["vigyazzhat"]
 tables = db["tables"]
-#tables.insert_one({"passwd" : "jelszo", "cards_in_play" : [[3, 3], [2, 2], [1, 1], [0, 0]], "players" : [{"hand" : []}, {"hand" : []}, {"hand" : []}, {"hand" : []}]})
-@app.route("/login/<username>", methods=['GET'])
-def login(username):
-    user = tables.insert_one({"username" : username, "hand" : []})
-    return str(user.inserted_id)
-    # create user in db:
-    # username (string)          - will be seen
-    # uuid     (hash)            - used to communicate with the server, not public
-    # hand     (array<int|byte>) - used to store cards as numbers/values, init to empty array
-    # returns uuid (player_id)
-    #pass
-    return "<p>oooooo</p>"
 
-@app.route("/dummy_create_table", methods=['POST'])
-def dummy_create_table(user_id, passwd):
-    return {"table_id" : "123"}
+def int_to_chars(integer):
+    #Számjegysorozatból karakterláncot generál
+    digit_to_letter = {
+        '0': 'J', '1': 'A', '2': 'B', '3': 'C', '4': 'D', '5': 'E', '6': 'F', '7': 'G', '8': 'H', '9': 'I'
+    }
+
+    string = str(integer)
+    string_out = ""
+
+    for char in string:
+        string_out += digit_to_letter[char]
+
+    return string_out
+
+def conv_to_hash(s):
+    return hashlib.sha256(s.encode()).hexdigest()
+
+
+def set_deck(game_id, deck):
+    tables.update_one({ "_id" : game_id }, { "$set" : { "deck" : deck } })
+
+def draw_from_deck(game_id, n):
+    deck = tables.find_one({"_id" : game_id })["deck"]
+    set_deck(game_id, deck[n:])
+    return deck[:n]
+
+def set_table(game_id):
+    tables.update_one({ "_id" : game_id }, { "$set" : { "cards_in_play" : [[draw_from_deck(game_id, 1)[0], None, None, None, None] for _ in range(4)] } })
+
+def get_players(game_id):
+    return tables.find_one({ "_id" : game_id })["players"].keys()
+
+def set_player_hand(game_id, player_id, cards):
+    tables.update_one( { "_id" : game_id }, { "$set" : { f"players.{player_id}.hand" : cards } } )
 
 @app.route("/create_table", methods=['POST'])
-def create_table(passwd):
-    # create table in db:
-    # game_id (hash)
-    # passwd  convert <string> to (hash) if present
-    # invite_string (short sequence of characters converted from the hash)
-    # table (2d array of int|byte)
-    # returns uuid (game_id)
-    pass
+def create_table():
+    owner = request.json["_playerName"]
+    passwd = request.json["_password"]
+
+    if tables.find_one( { 'owner' : conv_to_hash(owner) } ):
+        return {}
+
+    game = {
+            "passwd": conv_to_hash(passwd),
+            "owner": conv_to_hash(owner),
+            "invite_string": conv_to_hash(owner)[-5:],
+            "deck" : [],
+            "cards_in_play": [
+                [None, None, None, None, None, None],
+                [None, None, None, None, None, None],
+                [None, None, None, None, None, None],
+                [None, None, None, None, None, None]
+            ],
+            "players": {}
+        }
+    
+    game["players"][conv_to_hash(owner)] = {
+        "player_name": owner,
+        "hand": [],
+        "ready" : False,
+    }
+    game_id = tables.insert_one(game).inserted_id
+
+    ids = { 'table_id' : str(game_id), 'player_id' : conv_to_hash(owner), "inviteLink" : game["invite_string"]}
+
+    return ids
 
 @app.route("/join_game", methods=['POST'])
-def join_game(game_id, passwd):
-    # connect a player to a table in db
-    # returns other players' names
-    pass
+def join_game():
+    player = request.json["_playerName"]
+    inv_link = request.json["inviteLink"]
+    passwd = request.json["_password"]
+
+    ids = {}
+    game = tables.find_one( { "invite_string" : inv_link } )
+    if game["passwd"] == conv_to_hash(passwd):
+        joining_player = { conv_to_hash(player) : { "player_name" : player , "hand" : [], "ready" : False }}
+        tables.update_one( { "_id" : game['_id'] }, { "$set" : { f"players.{conv_to_hash(player)}" : joining_player[conv_to_hash(player)] } } )
+        ids = { "table_id" : str(game['_id']), "player_id" : conv_to_hash(player) }
+
+    return ids
+
+@app.route("/get_game_state/<tableId>", methods=['GET'])
+def get_game_state(tableId):
+    try:
+        game_id = ObjectId(tableId)
+    except:
+        return {}
+    
+    game = tables.find_one({"_id": game_id})
+    return { "players" : {player_info["player_name"]: len(player_info["hand"]) for player_info in game["players"].values()}, "cards_in_play" : game["cards_in_play"] }
+#           [ ,game["cards_in_play"]]
+
+#print(get_game_state(ObjectId("673593f95dab2d87358385f1")))
 
 @app.route("/ready", methods=['GET'])
 def ready(player_id):
@@ -48,24 +115,24 @@ def ready(player_id):
     # returns initial game state
     pass
 
-def start_game(game_id):
-    from random import shuffle
+@app.route("/start_game", methods=['POST'])
+def start_game():
+    game_id = ObjectId(request.json["tableId"])
+    player_id = request.json["playerId"]
+    if tables.find_one( { "_id" : game_id } )["owner"] == player_id:
+        from random import shuffle
 
-    deck = list(range(1,105))
-    shuffle(deck)
+        deck = list(range(1,105))
+        shuffle(deck)
 
-    set_deck(game_id, deck) # sets deck in the db
-    table = set_table(game_id, draw_from_deck(game_id, 20)) # draw_from_deck gets info from database, slice the deck accordingly, writes remaining deck back and return the drawn cards # set_table sets the 2d array of the table in the db with the gotten cards
-    players = get_players(db, game_id) # get player ids from db
-    for player_id in players:
-        hand = set_player_hand(db, player_id, draw_from_deck(game_id, 10)) # draw 10 card and give to player in db
-
-    # return game_state
-    pass
-
-def set_deck(game_id, deck):
-    
-    pass
+        set_deck(game_id, deck) # sets deck in the db
+        set_table(game_id) # draw_from_deck gets info from database, slice the deck accordingly, writes remaining deck back and return the drawn cards # set_table sets the 2d array of the table in the db with the gotten cards
+        
+        players = get_players(game_id) # get player ids from db
+        for player_id in players:
+            set_player_hand(game_id, player_id, draw_from_deck(game_id, 10)) # draw 10 card and give to player in db
+    return {"yes" : "you are"}
+#start_game(ObjectId("673580670d90add60e6f1a59"))
 
 if __name__  == '__main__':
     app.run()
